655450f97a7d7cb4c59d22417f7a9af02ed26112
Gyride <sebast.jar@gmail.com>
2018-04-23 11:39:23 -0400

    main import fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './stylesheets/index.css';
-import App from './components/App';
+import App from './components/Main';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Main />, document.getElementById('root'));
